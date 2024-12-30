8093710f4065629b1ceb7a985e3625192aa71d4a
BugHunter-X <askzenai@gmail.com>
2024-12-30 17:16:40 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,6 @@
+```javascript
+<div class="flex">
+  <div class="bg-red-500 p-4 w-1/2 flex-shrink-0 flex-grow-0"></div>
+  <div class="bg-blue-500 p-4 w-1/2 flex-shrink-0 flex-grow-0"></div>
+</div>
+```
